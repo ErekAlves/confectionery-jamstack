eb9b0fbbfff7c422af84e1878726b2d8f06ee705
jean Erek <jeanerekdev@gmail.com>
2023-11-17 16:01:49 -0300

    initial

---
+++ src/components/layout.js
@@ -0,0 +1,11 @@
+// import * as React from 'react'
+// import {link} from 'gatsby'
+
+
+// const Layout = () => {
+//     return (
+
+
+//     )
+
+// }
---
+++ src/pages/recipes.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const Recipes = () => {
+  return (
+    <main>
+      <h1>Teste</h1>
+      <p></p>
+    </main>
+  );
+};
+
+export default Recipes;
+
+export const Head = () => <title>Receitas Page</title>;
